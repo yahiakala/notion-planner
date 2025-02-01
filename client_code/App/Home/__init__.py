736344bd2bc7d835a209f415343e7548a44cbd3c
yahiakala <24422258+yahiakala@users.noreply.github.com>
2025-01-31 22:19:11 -0500

    simplified permission handling on reschedule

--- client_code/App/Home/__init__.py
+++ client_code/App/Home/__init__.py
@@ -19,27 +19,32 @@ class Home(HomeTemplate):
         # Get the max number of hours to schedule in a day (x)
         self.tenant = Global.tenant
 
+    def _set_loading_state(self, is_loading: bool) -> None:
+        """Helper to manage loading state of components"""
+        self.btn_refresh_today.enabled = not is_loading
+        self.btn_reschedule.enabled = not is_loading
+        self.cp_loading.visible = is_loading
+        self.ti_reschedule.interval = 2 if is_loading else 0
+
     def btn_reschedule_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        if 'schedule_tasks' not in Global.usertenant['plan_permissions']:
-            print(Global.usertenant['plan_permissions'])
-            alert('Upgrade to access this feature')
-        else:
-            c = confirm("Are you sure you want to reschedule all your tasks?")
-            if c:
-                try:
-                    _ = anvil.server.call_s("rebalance1_call", Global.tenant_id)
-                    self.btn_refresh_today.enabled = False
-                    self.btn_reschedule.enabled = False
-                    self.cp_loading.visible = True
-                    self.ti_reschedule.interval = 2
-                except anvil.server.PermissionDenied:
-                    alert('You do not have permission for this action. Please contact your administrator.')
-                except Exception as e:
-                    alert(str(e))
+        """Handle rescheduling of all tasks"""
+        if "schedule_tasks" not in Global.usertenant["plan_permissions"]:
+            alert("Upgrade to access this feature")
+            return
+
+        if not confirm("Are you sure you want to reschedule all your tasks?"):
+            return
+
+        try:
+            _ = anvil.server.call_s("rebalance1_call", Global.tenant_id)
+            self._set_loading_state(True)
+        except anvil.server.PermissionDenied:
+            alert(
+                "You do not have permission for this action. Please contact your administrator."
+            )
 
     def ti_reschedule_tick(self, **event_args):
-        """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+        """Check rebalancing status every interval"""
         print_timestamp("ti_reschedule_tick")
 
         with anvil.server.no_loading_indicator:
@@ -47,10 +52,7 @@ class Home(HomeTemplate):
                 "dt_bk_running", Global.tenant_id, "tenants", "rebalance1_single"
             ):
                 print_timestamp("ti_reschedule_tick: last one")
-                self.ti_reschedule.interval = 0
-                self.cp_loading.visible = False
-                self.btn_refresh_today.enabled = True
-                self.btn_reschedule.enabled = True
+                self._set_loading_state(False)
 
     def btn_settings_click(self, **event_args):
         """This method is called when the button is clicked"""
